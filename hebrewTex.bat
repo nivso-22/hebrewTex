86ebc07095780674b060fa36ee0c129c12429ac1
nivso-22 <122080004+nivso-22@users.noreply.github.com>
2025-05-11 15:02:16 +0300

    Add files via upload

--- hebrewTex.bat
+++ hebrewTex.bat
@@ -1,5 +1,6 @@
 @echo off
-pandoc "%~1" -o temp_output.tex --from docx --to latex --standalone --toc --number-sections --wrap=none --no-highlight
+pandoc --extract-media=. "%~1".docx -o output.md
+pandoc "%~1" -o temp_output.tex --from docx --to latex --standalone --toc --number-sections --wrap=none --no-highlight --extract-media=output.md
 python  C:\Users\converter.py temp_output.tex "%~2"
 xelatex -interaction=nonstopmode "%~2"
 pause
